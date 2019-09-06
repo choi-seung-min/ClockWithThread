90c1b9d0db00d8f3b8a23b6b21aaddbb91506482
seung min <3235tmdals@gmail.com>
2019-09-06 18:37:45 +0900

    home work finished(Thread)

--- app/src/main/java/com/example/undefinedhomework/MainActivity.kt
+++ app/src/main/java/com/example/undefinedhomework/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Message
+import androidx.constraintlayout.widget.ConstraintLayout
 import kotlinx.android.synthetic.main.activity_main.*
 import java.text.SimpleDateFormat
 import java.util.*
@@ -14,9 +15,17 @@ class MainActivity : AppCompatActivity() {
     lateinit var minute: String
     lateinit var second: String
 
+    lateinit var lpHour: ConstraintLayout.LayoutParams
+    lateinit var lpMinute: ConstraintLayout.LayoutParams
+    lateinit var lpSecond: ConstraintLayout.LayoutParams
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        lpHour = tv_hour.layoutParams as ConstraintLayout.LayoutParams
+        lpMinute = tv_minute.layoutParams as ConstraintLayout.LayoutParams
+        lpSecond = tv_second.layoutParams as ConstraintLayout.LayoutParams
 
         val handler = TimeHandler()
         timeCheck(handler)
@@ -29,6 +38,9 @@ class MainActivity : AppCompatActivity() {
                     hour = SimpleDateFormat("hh", Locale.KOREA).format(Date())
                     minute = SimpleDateFormat("mm", Locale.KOREA).format(Date())
                     second = SimpleDateFormat("ss", Locale.KOREA).format(Date())
+                    lpHour.circleAngle = 360/12*hour.toFloat()
+                    lpMinute.circleAngle = 360/60*minute.toFloat()
+                    lpSecond.circleAngle = 360/60*second.toFloat()
                     val message = handler.obtainMessage() as Message
                     handler.sendMessage(message)
                     Thread.sleep(1000)
@@ -43,6 +55,9 @@ class MainActivity : AppCompatActivity() {
             tv_hour.text = hour
             tv_minute.text = minute
             tv_second.text = second
+            tv_hour.layoutParams = lpHour
+            tv_minute.layoutParams = lpMinute
+            tv_second.layoutParams = lpSecond
         }
     }
 }
